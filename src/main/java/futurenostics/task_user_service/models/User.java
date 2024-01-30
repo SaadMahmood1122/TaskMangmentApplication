84c950fcbeea0b67e03a11e8838e5f651e263e5d
SaadMahmood1122 <sk8066709@gmail.com>
2024-01-31 00:10:25 +0500

    first commit

---
+++ src/main/java/futurenostics/task_user_service/models/User.java
@@ -0,0 +1,21 @@
+package futurenostics.task_user_service.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+    private String fullName;
+}
